04c518429fac5dd96795bb93e1f4b845d38998ff
Timothy Do <timothd4@uci.edu>
2022-03-06 17:51:16 -0800

    Update dips.py

--- src/dips.py
+++ src/dips.py
@@ -5,10 +5,15 @@
 import cv2
 import os
 import time
+import numpy as np
 
 #Converts the Image to Grayscale and Writes to It
 def grayscale(sourcePath,destPath):
 	cv2.imwrite(destPath,cv2.cvtColor(cv2.imread(sourcePath),cv2.COLOR_BGR2GRAY))
+	image = cv2.imread(sourcePath)
+	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+	gray = cv2.merge([gray,gray,gray])
+	cv2.imwrite(destPath,gray)
 
 #Converts a whole folder with images to Grayscale
 def grayscaleFolder(source,dest):
@@ -23,9 +28,10 @@ def grayscaleFolder(source,dest):
 def downScaleImage(sourcePath,destPath):
 	image = cv2.imread(sourcePath)
 	dim = image.shape
-	ar = dim[0]/dim[1]
-	res = 224
-	cv2.imwrite(destPath,cv2.resize(image,(res,round(ar*res))))
+	#ar = dim[0]/dim[1]
+	w = 256
+	h = 256
+	cv2.imwrite(destPath,cv2.resize(image,(w,h)))
 
 # Downscales an Entire Folder
 def downScaleFolder(source,dest):
@@ -42,6 +48,26 @@ def renameImages(path):
 		oldname, ext = os.path.splitext(image)
 		if not(os.path.exists(path + "photo"  + str(i) + ext)):
 			os.rename(path + image, path + "photo" + str(i) + ext)
+
+# Converts an Image into LAB format as a numpy array
+def img2lab(path):
+    img = cv2.imread(path)
+    lab = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+	# normalization from 0 to 100 
+    return ((lab / [100, 1, 1]) * [255, 1, 1]) - [0, 128, 128]
+
+# Creating Folder with 313 Classes
+def classCreate(path):
+	if(not os.path.exists(path)):
+		os.makedirs(path)
+	for i in range(313):
+		folder = path + "\\" + str(i) + "\\"
+		if(not os.path.exists(folder)): 
+			os.makedirs(folder)
+	
+	
+
+# Converts an Image into LAB format, then saves it as a numpy array
 
 #Prepare Folder of Images to Train with the Data (path is the image where image dataset is located)
 # Assume Training Path and Validation Path has color and gray folders within them
@@ -77,12 +103,17 @@ def prepareImageSet(inputPath,grayPath,trainPath,valPath,traintoVal):
 	print("Segmenting Images: " + str(current-prev) + " s")
 	prev = current
 	
-	#Makes Grayscale Counterpart
-	grayscaleFolder(inputPath,grayPath)
-	current = time.time()
-	print("Grayscale Images: " + str(current-prev) + " s")
-	prev = current
+	# #Makes Grayscale Counterpart
+	# grayscaleFolder(inputPath,grayPath)
+	# current = time.time()
+	# print("Grayscale Images: " + str(current-prev) + " s")
+	# prev = current
 
+	#Creating Classification Folders
+	classCreate(trainPath)
+	classCreate(valPath)
+
+	#Sorting by knn nearest neighbors
 	i = 1
 	for file in os.listdir(inputPath):
 		# Training
