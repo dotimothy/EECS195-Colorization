0a594bfe5dfae7dc439d30eafee4547226eb53c4
Timothy Do <timothd4@uci.edu>
2022-03-08 23:31:00 -0800

    Update dips.py

--- src/dips.py
+++ src/dips.py
@@ -8,6 +8,7 @@ import time
 import numpy as np
 
 #Converts the Image to Grayscale and Writes to It
+# DEPRECATED
 def grayscale(sourcePath,destPath):
 	cv2.imwrite(destPath,cv2.cvtColor(cv2.imread(sourcePath),cv2.COLOR_BGR2GRAY))
 	image = cv2.imread(sourcePath)
@@ -16,6 +17,7 @@ def grayscale(sourcePath,destPath):
 	cv2.imwrite(destPath,gray)
 
 #Converts a whole folder with images to Grayscale
+# DEPRECATED
 def grayscaleFolder(source,dest):
 	# Loops through directory for images
 	for file in os.listdir(source):
@@ -29,8 +31,8 @@ def downScaleImage(sourcePath,destPath):
 	image = cv2.imread(sourcePath)
 	dim = image.shape
 	#ar = dim[0]/dim[1]
-	w = 256
-	h = 256
+	w = 224
+	h = 224
 	cv2.imwrite(destPath,cv2.resize(image,(w,h)))
 
 # Downscales an Entire Folder
@@ -57,6 +59,7 @@ def img2lab(path):
     return ((lab / [100, 1, 1]) * [255, 1, 1]) - [0, 128, 128]
 
 # Creating Folder with 313 Classes
+# DEPRECATEAD
 def classCreate(path):
 	if(not os.path.exists(path)):
 		os.makedirs(path)
@@ -64,8 +67,21 @@ def classCreate(path):
 		folder = path + "\\" + str(i) + "\\"
 		if(not os.path.exists(folder)): 
 			os.makedirs(folder)
-	
-	
+
+# Converts a Folder of Images to L & AB numpy
+def setToLab(path):
+	np_L = []
+	np_ab = []
+	for img in os.listdir(path):
+		isPicture = img.endswith(".jpg") or img.endswith(".png") or img.endswith(".JPG") or img.endswith(".PNG")
+		if isPicture == True:
+			lab = img2lab(path + img)
+			L = lab[:,:,0]
+			ab = lab[:,:,1:2]
+			np_L.append(L)
+			np_ab.append(ab)
+	np.save(path + 'l.npy',np_L)
+	np.save(path + 'ab.npy',np_ab)	
 
 # Converts an Image into LAB format, then saves it as a numpy array
 
@@ -73,11 +89,7 @@ def classCreate(path):
 # Assume Training Path and Validation Path has color and gray folders within them
 # Parameters: 
 # inputPath: Path to Where All The Original Colorized Images Are
-# grayPath: Path to Where All the Gray Images will Go
-# trainPath: Path to where training images are
-# valPath: Path to where validation images are 
-# traintoVal: The decimal percentage of the training to validation ratio 
-def prepareImageSet(inputPath,grayPath,trainPath,valPath,traintoVal):
+def prepareImageSet(inputPath):
 	#Initialize
 	imageNumber = len(os.listdir(inputPath))
 	prev = time.time()
@@ -89,7 +101,7 @@ def prepareImageSet(inputPath,grayPath,trainPath,valPath,traintoVal):
 	print("Rename Images: " + str(current-prev) + " s")
 	prev = current
 	
-	#Downscale all Images in Matlab to 1080p
+	#Downscale all Images in Matlab to 224 by 224
 	downScaleFolder(inputPath,inputPath)
 	current = time.time()
 	print("Downscale Images: " + str(current-prev) + " s")
@@ -103,29 +115,10 @@ def prepareImageSet(inputPath,grayPath,trainPath,valPath,traintoVal):
 	print("Segmenting Images: " + str(current-prev) + " s")
 	prev = current
 	
-	# #Makes Grayscale Counterpart
-	# grayscaleFolder(inputPath,grayPath)
-	# current = time.time()
-	# print("Grayscale Images: " + str(current-prev) + " s")
-	# prev = current
-
-	#Creating Classification Folders
-	classCreate(trainPath)
-	classCreate(valPath)
-
-	#Sorting by knn nearest neighbors
-	i = 1
-	for file in os.listdir(inputPath):
-		# Training
-		move = trainPath
-		if(i > round(imageNumber*traintoVal)): #Validation
-			move = valPath 
-		os.rename(inputPath + file,move + "\\color\\" + file)
-		os.rename(grayPath + file,move + "\\gray\\" + file)
-		i = i + 1
 	
 	current = time.time()
-	print("Images to Model Folders: " + str(current-prev) + " s")
+	setToLab(inputPath)
+	print("Image Set to Numpy Folders: " + str(current-prev) + " s")
 	prev = current
 		
 			
